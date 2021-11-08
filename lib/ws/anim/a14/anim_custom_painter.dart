bf5aa3c1a2e3353a414233962a8a443b5698cc9f
svran <924633827@qq.com>
2021-11-08 21:55:30 +0800

    _ 更新下雪.

--- lib/ws/anim/a14/anim_custom_painter.dart
+++ lib/ws/anim/a14/anim_custom_painter.dart
@@ -12,7 +12,7 @@ class AminCustomPainterDemoPage extends StatefulWidget {
 class _AminCustomPainterDemoPageState extends State<AminCustomPainterDemoPage> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
-  List<Snowflake> list = List.generate(100, (index) => Snowflake());
+  List<Snowflake> list = List.generate(150, (index) => Snowflake());
 
   @override
   void initState() {
@@ -74,6 +74,7 @@ class MyPainter extends CustomPainter {
   final whitePaint = Paint()..color = Colors.white;
   final blackPointPaint = Paint()..color = Colors.black;
   final blackNosePaint = Paint()..color = Colors.orangeAccent;
+  final hatPaint = Paint()..color = Colors.red;
   final blackLinePaint = Paint()
     ..strokeWidth = 1
     ..color = Colors.black
@@ -81,6 +82,12 @@ class MyPainter extends CustomPainter {
   final blackLineHandPaint = Paint()
     ..strokeWidth = 4
     ..color = Colors.black
+    ..strokeCap = StrokeCap.round
+    ..style = PaintingStyle.stroke;
+  final fingerPaint = Paint()
+    ..strokeWidth = 2
+    ..color = Colors.black
+    ..strokeCap = StrokeCap.round
     ..style = PaintingStyle.stroke;
 
   @override
@@ -88,12 +95,15 @@ class MyPainter extends CustomPainter {
     // logger.d("Svran: Flutter -> ${size.width} , ${size.height}");
     // Offset(size.width / 2, size.height / 2) 等于 size.center(Offset.zero)
 
-    // 头
-    canvas.drawCircle(size.bottomCenter(const Offset(0, -250)), 60.0, whitePaint);
-
     // 身子
     canvas.drawOval(
         Rect.fromCenter(center: size.bottomCenter(const Offset(0, -100)), width: 200, height: 250), whitePaint);
+
+    // 围巾
+    canvas.drawCircle(size.bottomCenter(const Offset(0, -230)), 60.0, hatPaint);
+
+    // 头
+    canvas.drawCircle(size.bottomCenter(const Offset(0, -250)), 60.0, whitePaint);
 
     // 眼珠子两颗
     canvas.drawCircle(size.bottomCenter(const Offset(-20, -270)), 5, blackPointPaint);
@@ -115,14 +125,40 @@ class MyPainter extends CustomPainter {
         size.bottomCenter(const Offset(-50, -180)), size.bottomCenter(const Offset(-130, -230)), blackLineHandPaint);
     canvas.drawLine(
         size.bottomCenter(const Offset(50, -180)), size.bottomCenter(const Offset(130, -230)), blackLineHandPaint);
+    // 左手手指
+    var leftStartPoint = size.bottomCenter(const Offset(-130, -230));
+    canvas.drawLine(leftStartPoint, Offset(leftStartPoint.dx - 10, leftStartPoint.dy - 15), fingerPaint);
+    canvas.drawLine(leftStartPoint, Offset(leftStartPoint.dx - 15, leftStartPoint.dy - 10), fingerPaint);
+    canvas.drawLine(leftStartPoint, Offset(leftStartPoint.dx - 15, leftStartPoint.dy), fingerPaint);
+    // 右手手指
+    var rightStartPoint = size.bottomCenter(const Offset(130, -230));
+    canvas.drawLine(rightStartPoint, Offset(rightStartPoint.dx + 10, rightStartPoint.dy + 15), fingerPaint);
+    canvas.drawLine(rightStartPoint, Offset(rightStartPoint.dx + 15, rightStartPoint.dy + 10), fingerPaint);
+    canvas.drawLine(rightStartPoint, Offset(rightStartPoint.dx + 15, rightStartPoint.dy), fingerPaint);
 
     // 扣子三颗
-    canvas.drawCircle(size.bottomCenter(const Offset(0, -110)), 5, blackLinePaint);
-    canvas.drawCircle(size.bottomCenter(const Offset(0, -140)), 5, blackLinePaint);
-    canvas.drawCircle(size.bottomCenter(const Offset(0, -170)), 5, blackLinePaint);
+    double buttonBaseHeight = -90.0;
+    canvas.drawCircle(size.bottomCenter(Offset(0, buttonBaseHeight)), 5, blackLinePaint);
+    canvas.drawCircle(size.bottomCenter(Offset(0, buttonBaseHeight - 30)), 5, blackLinePaint);
+    canvas.drawCircle(size.bottomCenter(Offset(0, buttonBaseHeight - 60)), 5, blackLinePaint);
 
     // 鼻子
     canvas.drawCircle(size.bottomCenter(const Offset(0, -255)), 5, blackNosePaint);
+    var nosePath = Path();
+    var nPoint = size.bottomCenter(Offset(0, -255));
+    nosePath.moveTo(nPoint.dx, nPoint.dy - 5); // 起点
+    nosePath.relativeLineTo(-20, 5); // 左边顶点
+    nosePath.relativeLineTo(20, 5); // 下边顶点
+    canvas.drawPath(nosePath, blackNosePaint);
+
+    // 帽子
+    canvas.drawCircle(size.bottomCenter(Offset(50, -380)), 10, hatPaint);
+    var hatPath = Path();
+    var hatPoint = size.bottomCenter(Offset(50, -380));
+    hatPath.moveTo(hatPoint.dx, hatPoint.dy);
+    hatPath.relativeLineTo(-60, 70);
+    hatPath.relativeLineTo(60, 28);
+    canvas.drawPath(hatPath, hatPaint);
 
     // 雪花
     for (var snowflake in snowflakes) {
@@ -142,7 +178,7 @@ class Snowflake {
   double w = 600, h = 800;
   double x = Random().nextDouble() * 600;
   double y = Random().nextDouble() * 800;
-  double radius = Random().nextDouble() * 2 + 4;
+  double radius = Random().nextDouble() * 1 + 3;
   double velocity = Random().nextDouble() * 4 + 2;
 
   fall() {
